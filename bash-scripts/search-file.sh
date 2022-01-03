1953f859e658e05a7b6f081e44bbcc9321f37fe9
Nick F <independence.training12@gmail.com>
2022-01-03 15:13:46 +0100

    Improved search scripts

--- bash-scripts/search-file.sh
+++ bash-scripts/search-file.sh
@@ -2,29 +2,37 @@
 
 # Script a recursive, case insensitive search for a file with a certain name using wildcards
 #
-# Usage: /absolute/path/to/search-file.sh [<directory>]
+# Usage: /absolute/path/to/search-file.sh [<directory>] [<editor command>]
 # Paramater defaults to ./src. If using wildcards, surround it with quotes
 #
 # Will prompt for search string
 #
 # In ~/.bashrc you could add the alias:
 # alias sf="/abosolute/path/to/search-file.sh"
-#    (sf abbreviation for search file, not sciencd fiction)
+#    (sf abbreviation for search file, not science fiction)
+
+if [ "$1" = "" ]
+then
+	DIR="./src/"
+else
+	DIR=$1
+fi
+EDITOR=$2
 
 echo "Input string to search for:"
 read SEARCH
 
 if [ "$SEARCH" = "" ]
 then
-	echo "Usage: /absolute/path/to/search-file.sh [<directory>]"
+	echo "Usage: /absolute/path/to/search-file.sh [<directory>] [<editor command>]"
 	exit 1
 fi
 
-if [ "$1" = "" ]
-then
-	DIR="./src/*"
-else
-	DIR=$1
-fi
 
-find ./src -iname "$SEARCH"
+if [ "$EDITOR" = "" ]
+then
+	find $DIR -iname "*$SEARCH*" -type f
+else
+	RESULT=( $( find $DIR -iname "*$SEARCH*" -type f ) ) # Results to array
+	$EDITOR "${RESULT[@]/#/}" # Spread array to parameters
+fi
--- bash-scripts/search.sh
+++ bash-scripts/search.sh
@@ -2,7 +2,7 @@
 
 # Script to recursively search for a string using grep
 #
-# Usage: /absolute/path/to/search.sh [<directory>]
+# Usage: /absolute/path/to/search.sh [<directory>]  [<editor command>]
 # Paramater defaults to ./src. If using wildcards, surround it with quotes
 #
 # Will prompt for search string
@@ -11,27 +11,43 @@
 # alias s="/abosolute/path/to/search.sh"
 #    (s abbreviation for search)
 
+if [ "$1" = "" ]
+then
+	DIR="./src/"
+else
+	DIR=$1
+fi
+EDITOR=$2
+
 echo "Input string to search for:"
 read SEARCH
 
 if [ "$SEARCH" = "" ]
 then
-	echo "Usage: /absolute/path/to/search.sh [<directory>]"
+	echo "Usage: /absolute/path/to/search.sh [<directory>]  [<editor command>]"
 	exit 1
-fi
-
-if [ "$1" = "" ]
-then
-	PAR="./src"
-else
-	PAR=$1
 fi
 
 COLOR=never # never, always or auto
 
-grep -inr --color=$COLOR "$SEARCH" $PAR
-
-# Options:
-# - i case insensitive
-# - r recursive
-# - n line number
+if [ "$EDITOR" = "" ]
+then
+	grep -inr --color=$COLOR "$SEARCH" $DIR*
+	# Options:
+	# - i case insensitive
+	# - r recursive
+	# - n line number
+else
+	RESULT=( $( grep -r -H "$SEARCH" $DIR* | sort -t: -u -k1,1 | cut -d: -f1 ) ) # Results to array
+	# Grep:
+	# - resursive (-r),
+	# - get file names (-H)
+	# Sort:
+	# - use : as the delimiter (-t:),
+	# - sort based on the first field only (-k1,1 - ),
+	# - remove duplicates (-u)
+	# Cut:
+	# - delimiter (-d:),
+	# - get first field, filename only (-f1)
+	$EDITOR "${RESULT[@]/#/}" # Spread array to parameters
+fi
